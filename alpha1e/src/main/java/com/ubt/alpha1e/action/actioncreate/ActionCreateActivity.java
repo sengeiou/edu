dba3b9cd7a9e5fac27e6883a2198e163f9e0463c
“马伟民” <weimin.ma@ubtrobot.com>
2017-11-14 17:27:39 +0800

    [需求]动作编辑页面优化

---
+++ alpha1e/src/main/java/com/ubt/alpha1e/action/actioncreate/ActionCreateActivity.java
@@ -0,0 +1,14 @@
+package com.ubt.alpha1e.action.actioncreate;
+
+
+import com.ubt.alpha1e.mvp.MVPBaseActivity;
+
+
+/**
+ * MVPPlugin
+ *  邮箱 784787081@qq.com
+ */
+
+public class ActionCreateActivity extends MVPBaseActivity<ActionCreateContract.View, ActionCreatePresenter> implements ActionCreateContract.View {
+
+}
