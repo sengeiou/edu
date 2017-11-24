a9e97437a66092dbbcddc9682e390fa3dfd8776e
李海 <hai.li@ubtrobot.com>
2017-11-24 17:01:20 +0800

    [V1.0.0.3]添加遥控器代码

---
+++ alpha1e/src/main/java/com/ubt/alpha1e/mainremote/MainRemoteActivity.java
@@ -0,0 +1,33 @@
+package com.ubt.alpha1e.mainremote;
+
+
+import com.ubt.alpha1e.mvp.MVPBaseActivity;
+
+
+/**
+ * MVPPlugin
+ *  邮箱 784787081@qq.com
+ */
+
+public class MainRemoteActivity extends MVPBaseActivity<MainRemoteContract.View, MainRemotePresenter> implements MainRemoteContract.View {
+
+    @Override
+    protected void initUI() {
+
+    }
+
+    @Override
+    protected void initControlListener() {
+
+    }
+
+    @Override
+    protected void initBoardCastListener() {
+
+    }
+
+    @Override
+    public int getContentViewId() {
+        return 0;
+    }
+}
