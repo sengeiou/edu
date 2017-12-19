df54065af8c8e93588d547c6202327714fa6c036
李海 <hai.li@ubtrobot.com>
2017-12-19 10:54:25 +0800

    [V1.0.0.9]添加行为习惯养成模块代码

---
+++ alpha1e/src/main/java/com/ubt/alpha1e/behaviorhabits/model/HabitsEvent.java
@@ -0,0 +1,35 @@
+package com.ubt.alpha1e.behaviorhabits.model;
+
+/**
+ * Created by Administrator on 2017/12/18.
+ */
+
+public class HabitsEvent {
+    private String eventId;
+    private String eventName;
+    private String eventTime;
+
+    public String getEventId() {
+        return eventId;
+    }
+
+    public void setEventId(String eventId) {
+        this.eventId = eventId;
+    }
+
+    public String getEventName() {
+        return eventName;
+    }
+
+    public void setEventName(String eventName) {
+        this.eventName = eventName;
+    }
+
+    public String getEventTime() {
+        return eventTime;
+    }
+
+    public void setEventTime(String eventTime) {
+        this.eventTime = eventTime;
+    }
+}
