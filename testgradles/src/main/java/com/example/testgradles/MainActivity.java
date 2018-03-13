de612249e13fdcc70f4a1d97c06af5bd12a915f4
李海 <hai.li@ubtrobot.com>
2018-03-13 17:26:23 +0800

    [V2.1.2.2]去掉Maven仓库测试功能编译

--- testgradles/src/main/java/com/example/testgradles/MainActivity.java
+++ testgradles/src/main/java/com/example/testgradles/MainActivity.java
@@ -3,10 +3,9 @@ package com.example.testgradles;
 import android.app.Activity;
 import android.os.Bundle;
 
-import com.ubtechinc.base.PublicInterface;
 
 
-public class MainActivity extends Activity implements PublicInterface.BlueToothInteracter{
+public class MainActivity extends Activity {
 
 
     @Override
@@ -15,23 +14,5 @@ public class MainActivity extends Activity implements PublicInterface.BlueToothI
         setContentView(R.layout.activity_main);
     }
 
-    @Override
-    public void onReceiveData(String s, byte b, byte[] bytes, int i) {
 
-    }
-
-    @Override
-    public void onSendData(String s, byte[] bytes, int i) {
-
-    }
-
-    @Override
-    public void onConnectState(boolean b, String s) {
-
-    }
-
-    @Override
-    public void onDeviceDisConnected(String s) {
-
-    }
 }
