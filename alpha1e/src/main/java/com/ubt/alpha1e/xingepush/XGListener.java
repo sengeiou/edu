cc545e8af1b7e470c357b288d03efa11422a8bcb
liuhai <leon.liu@ubtrobot.com>
2018-03-16 16:43:24 +0800

    [修复]通知栏目通知消息点击事件

--- alpha1e/src/main/java/com/ubt/alpha1e/xingepush/XGListener.java
+++ alpha1e/src/main/java/com/ubt/alpha1e/xingepush/XGListener.java
@@ -22,43 +22,57 @@ public class XGListener implements IXGListener {
 
     @Override
     public void onRegisterResult(Context context, int i, XGPushRegisterResult xgPushRegisterResult) {
-        Log.d(TAG,"onRegisterResult");
+        Log.d(TAG, "onRegisterResult");
         EventBus.getDefault().post(xgPushRegisterResult);
     }
 
     @Override
     public void onUnregisterResult(Context context, int i) {
-        Log.d(TAG,"onUnregisterResult");
+        Log.d(TAG, "onUnregisterResult");
         EventBus.getDefault().post(i);
     }
 
     @Override
     public void onSetTagResult(Context context, int i, String s) {
-        Log.d(TAG,"onSetTagResult");
+        Log.d(TAG, "onSetTagResult");
         EventBus.getDefault().post(s);
     }
 
     @Override
     public void onDeleteTagResult(Context context, int i, String s) {
-        Log.d(TAG,"onDeleteTagResult");
+        Log.d(TAG, "onDeleteTagResult");
         EventBus.getDefault().post(s);
     }
 
     @Override
     public void onTextMessage(Context context, XGPushTextMessage xgPushTextMessage) {
-        Log.d(TAG,"onTextMessage title"+xgPushTextMessage.getTitle() +"content  "+xgPushTextMessage.getContent());
+        Log.d(TAG, "onTextMessage title" + xgPushTextMessage.getTitle() + "content  " + xgPushTextMessage.getContent());
         EventBus.getDefault().post(xgPushTextMessage);
     }
 
     @Override
     public void onNotifactionClickedResult(Context context, XGPushClickedResult xgPushClickedResult) {
-        Log.d(TAG,"onNotifactionClickedResult");
+        Log.d(TAG, "onNotifactionClickedResult"+xgPushClickedResult.toString());
+        String text = "";
+        if (xgPushClickedResult.getActionType() == XGPushClickedResult.NOTIFACTION_CLICKED_TYPE) {
+            // 通知在通知栏被点击啦。。。。。
+            // APP自己处理点击的相关动作
+            // 这个动作可以在activity的onResume也能监听，请看第3点相关内容
+            text = "通知被打开 :";
+        } else if (xgPushClickedResult.getActionType() == XGPushClickedResult.NOTIFACTION_DELETED_TYPE) {
+            // 通知被清除啦。。。。
+            // APP自己处理通知被清除后的相关动作
+            text = "通知被清除 :";
+        }
+        Log.d(TAG, "onNotifactionClickedResult==" + text);
         EventBus.getDefault().post(xgPushClickedResult);
     }
 
     @Override
     public void onNotifactionShowedResult(Context context, XGPushShowedResult xgPushShowedResult) {
-        Log.d(TAG,"onNotifactionShowedResult"+xgPushShowedResult.getContent());
+        Log.d(TAG, "onNotifactionShowedResult" + xgPushShowedResult.getContent());
         EventBus.getDefault().post(xgPushShowedResult);
+
+
     }
 }
