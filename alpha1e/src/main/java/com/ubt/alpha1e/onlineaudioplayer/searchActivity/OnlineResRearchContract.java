065fc69f60b92b8947d27bc9ee17eb8060b5ad05
李海 <hai.li@ubtrobot.com>
2018-04-26 20:06:33 +0800

    [V1.0.3.2]提交在线资源代码

---
+++ alpha1e/src/main/java/com/ubt/alpha1e/onlineaudioplayer/searchActivity/OnlineResRearchContract.java
@@ -0,0 +1,19 @@
+package com.ubt.alpha1e.onlineaudioplayer.searchActivity;
+
+import com.ubt.alpha1e.mvp.BasePresenter;
+import com.ubt.alpha1e.mvp.BaseView;
+
+/**
+ * MVPPlugin
+ *  邮箱 784787081@qq.com
+ */
+
+public class OnlineResRearchContract {
+    interface View extends BaseView {
+        
+    }
+
+    interface  Presenter extends BasePresenter<View> {
+        
+    }
+}
---
+++ alpha1e/src/main/java/com/ubt/alpha1e/onlineaudioplayer/searchActivity/OnlineResRearchPresenter.java
@@ -0,0 +1,12 @@
+package com.ubt.alpha1e.onlineaudioplayer.searchActivity;
+
+import com.ubt.alpha1e.mvp.BasePresenterImpl;
+
+/**
+ * MVPPlugin
+ *  邮箱 784787081@qq.com
+ */
+
+public class OnlineResRearchPresenter extends BasePresenterImpl<OnlineResRearchContract.View> implements OnlineResRearchContract.Presenter{
+    
+}
